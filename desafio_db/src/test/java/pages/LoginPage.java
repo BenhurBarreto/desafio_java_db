3b24928c72add34c89af1b17450deec8bed3172e
Benhur <benhurbbs@gmail.com>
2024-03-06 20:44:28 -0300

    Criação dos métodos para manipulação dos xpaths/elementos

--- desafio_db/src/test/java/pages/LoginPage.java
+++ desafio_db/src/test/java/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import utility.BrowserDriver;
 
 public class LoginPage extends BrowserDriver {
@@ -7,4 +8,16 @@ public class LoginPage extends BrowserDriver {
     public static String password_xpath = "// div[2] / div[1] / input[@type='password']";
     public static String acessar_xpath = "//button[text()='Acessar']";
     public static String registrar_xpath = "//button[text()='Registrar']";
+    public static void sendkeys_email(String email){
+        driver.findElement(By.xpath(email_xpath)).sendKeys(email);
+    }
+    public static void sendkeys_password(String password){
+        driver.findElement(By.xpath(password_xpath)).sendKeys(password);
+    }
+    public static void click_acessar(){
+        driver.findElement(By.xpath(acessar_xpath)).click();
+    }
+    public static void click_registrar(){
+        driver.findElement(By.xpath(registrar_xpath)).click();
+    }
 }
