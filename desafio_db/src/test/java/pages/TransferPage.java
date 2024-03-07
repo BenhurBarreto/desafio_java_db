bb20718c859a0cbc17e58e5bb4055e125074ee3f
Benhur <benhurbbs@gmail.com>
2024-03-06 21:05:00 -0300

    Criação dos métodos para manipulação dos xpaths/elementos da Transfer Page

--- desafio_db/src/test/java/pages/TransferPage.java
+++ desafio_db/src/test/java/pages/TransferPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import utility.BrowserDriver;
 
 public class TransferPage extends BrowserDriver {
@@ -10,4 +11,25 @@ public class TransferPage extends BrowserDriver {
     public static String transferir_agora_xpath = "//button[text()='Transferir agora']";
     public static String sair_xpath = "//a[@id='btnExit']";
     public static String voltar_xpath = "//a[@id='btnBack']";
+    public static void sendkeys_num_conta(String num_conta){
+        driver.findElement(By.xpath(num_conta_xpath)).sendKeys(num_conta);
+    }
+    public static void sendkeys_digito_conta(String digito_conta){
+        driver.findElement(By.xpath(digito_conta_xpath)).sendKeys(digito_conta);
+    }
+    public static void sendkeys_valor(String valor){
+        driver.findElement(By.xpath(valor_xpath)).sendKeys(valor);
+    }
+    public static void sendkeys_descricao(String descricao){
+        driver.findElement(By.xpath(descricao_xpath)).sendKeys(descricao);
+    }
+    public static void click_transferir_agora(){
+        driver.findElement(By.xpath(transferir_agora_xpath)).click();
+    }
+    public static void click_sair(){
+        driver.findElement(By.xpath(sair_xpath)).click();
+    }
+    public static void click_voltar(){
+        driver.findElement(By.xpath(voltar_xpath)).click();
+    }
 }
